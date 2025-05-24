c34cf1e118a43050a9d2b1b4dbc9ceb86c655ff4
MadOne <madone85@googlemail.com>
2025-05-24 20:32:55 +0200

    first upload

---
+++ src/lights.rs
@@ -0,0 +1,276 @@
+use std::sync::{Arc, Mutex};
+
+use crate::x1;
+
+pub struct SwitchedLight<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub name: String,
+    pub switch: Switch<'a>,
+}
+
+pub struct DimmedLight<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub name: String,
+    pub switch: &'a Switch<'a>,
+    pub dimmer: &'a Dimm<'a>,
+}
+#[derive(Clone)]
+pub struct TunableLight<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub name: String,
+    pub switch: Switch<'a>,
+    pub dimmer: Dimm<'a>,
+    pub tuner: ColorTemp<'a>,
+}
+
+pub struct SwitchableLights<'a> {
+    switchable: Arc<Mutex<Vec<SwitchedLight<'a>>>>,
+}
+impl<'a> SwitchableLights<'a> {
+    pub fn new() -> Self {
+        let vector: Vec<SwitchedLight> = vec![];
+        let mutex = Mutex::new(vector);
+        let arc = Arc::new(mutex);
+        SwitchableLights { switchable: arc }
+    }
+    pub fn add(&'a self, light: SwitchedLight<'a>) {
+        self.switchable.lock().unwrap().push(light);
+    }
+    pub fn list(&self) {
+        println!("Switchable Lights:");
+        for (index, switchable) in self.switchable.lock().unwrap().iter().enumerate() {
+            println!("{index}: {}", switchable.name);
+        }
+    }
+    pub async fn switch_on(&'a self, id: u8) {
+        self.switchable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .on()
+            .await;
+    }
+    pub async fn switch_off(&'a self, id: u8) {
+        self.switchable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .off()
+            .await;
+    }
+}
+
+pub struct DimmableLights<'a> {
+    pub dimmable: Arc<Mutex<Vec<DimmedLight<'a>>>>,
+}
+impl<'a> DimmableLights<'a> {
+    pub fn new() -> Self {
+        let vector: Vec<DimmedLight> = vec![];
+        let mutex = Mutex::new(vector);
+        let arc = Arc::new(mutex);
+        DimmableLights { dimmable: arc }
+    }
+    pub fn add(&'a self, light: DimmedLight<'a>) {
+        self.dimmable.lock().unwrap().push(light);
+    }
+    pub fn list(&self) {
+        println!("Dimmable Lights:");
+        for (index, dimmable) in self.dimmable.lock().unwrap().iter().enumerate() {
+            println!("{index}: {}", dimmable.name);
+        }
+    }
+    pub async fn switch_on(&'a self, id: u8) {
+        self.dimmable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .on()
+            .await;
+    }
+    pub async fn switch_off(&'a self, id: u8) {
+        self.dimmable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .off()
+            .await;
+    }
+    pub async fn dimm(&'a self, id: u8, val: u16) {
+        self.dimmable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .dimmer
+            .set_val(val)
+            .await;
+    }
+}
+pub struct TunableLights<'a> {
+    pub tuneable: Arc<Mutex<Vec<TunableLight<'a>>>>,
+}
+impl<'a> TunableLights<'a> {
+    pub fn new() -> Self {
+        let vector: Vec<TunableLight> = vec![];
+        let mutex = Mutex::new(vector);
+        let arc = Arc::new(mutex);
+        TunableLights { tuneable: arc }
+    }
+    pub fn add(&'a self, light: TunableLight<'a>) {
+        self.tuneable.lock().unwrap().push(light);
+    }
+    pub fn list(&self) {
+        println!("Tunable Lights:");
+        for (index, tuneable) in self.tuneable.lock().unwrap().iter().enumerate() {
+            println!("{index}: {}", tuneable.name);
+        }
+    }
+    pub async fn switch_on(&'a self, id: u8) {
+        self.tuneable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .on()
+            .await;
+    }
+    pub async fn switch_off(&'a self, id: u8) {
+        self.tuneable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .switch
+            .off()
+            .await;
+    }
+    pub async fn dimm(&'a self, id: u8, val: u16) {
+        self.tuneable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .dimmer
+            .set_val(val)
+            .await;
+    }
+    pub async fn tune(&'a self, id: u8, color: u16) {
+        self.tuneable
+            .lock()
+            .unwrap()
+            .get(id as usize)
+            .unwrap()
+            .tuner
+            .set_val(color)
+            .await;
+    }
+}
+pub struct Lights<'a> {
+    pub switchable: SwitchableLights<'a>,
+    pub dimmable: DimmableLights<'a>,
+    pub tunable: TunableLights<'a>,
+}
+impl Lights<'_> {
+    pub fn new() -> Self {
+        Lights {
+            switchable: SwitchableLights::new(),
+            dimmable: DimmableLights::new(),
+            tunable: TunableLights::new(),
+        }
+    }
+    pub fn list(&self) {
+        self.switchable.list();
+        self.dimmable.list();
+        self.tunable.list();
+    }
+}
+
+pub struct ColoredLight {}
+
+#[derive(Clone)]
+pub struct Switch<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub uid: String,
+    pub val: u32,
+}
+impl Switch<'_> {
+    pub async fn on(&self) {
+        self.x1
+            .set_value(self.uid.clone(), 1)
+            .await
+            .expect("error switching on light");
+    }
+    pub async fn off(&self) {
+        self.x1
+            .set_value(self.uid.clone(), 0)
+            .await
+            .expect("error switching on light");
+    }
+    pub async fn refresh_val(&mut self) {
+        self.x1
+            .get_value(self.uid.clone())
+            .await
+            .expect("Error refreshing value");
+    }
+}
+
+#[derive(Clone)]
+pub struct Dimm<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub uid: String,
+    pub val: u32,
+}
+impl Dimm<'_> {
+    pub async fn set_val(&self, val: u16) {
+        self.x1
+            .set_value(self.uid.clone(), val)
+            .await
+            .expect("error switching on light");
+    }
+
+    pub async fn refresh_val(&mut self) {
+        self.x1
+            .get_value(self.uid.clone())
+            .await
+            .expect("Error refreshing value");
+    }
+}
+
+#[derive(Clone)]
+pub struct ColorTemp<'a> {
+    pub x1: &'a x1::X1<'a>,
+    pub uid: String,
+    pub val: u32,
+}
+
+impl ColorTemp<'_> {
+    pub async fn set_val(&self, val: u16) {
+        self.x1
+            .set_value(self.uid.clone(), val)
+            .await
+            .expect("error switching on light");
+    }
+
+    pub async fn refresh_val(&mut self) {
+        self.x1
+            .get_value(self.uid.clone())
+            .await
+            .expect("Error refreshing value");
+    }
+}
+
+/*
+pub struct Color {
+    uid: String,
+    val: u32,
+}
+*/
---
+++ src/x1.rs
@@ -0,0 +1,229 @@
+use crate::lights::*;
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct X1<'a> {
+    addr: String,
+    user: String,
+    password: String,
+    client: reqwest::Client,
+    token: Arc<Mutex<Option<String>>>,
+    ui: Arc<Mutex<Option<UiResponse>>>,
+    pub lights: Lights<'a>,
+}
+
+impl<'a> X1<'a> {
+    pub fn new(addr: &str, user: &str, password: &str) -> Self {
+        let client: reqwest::Client = reqwest::Client::builder()
+            .danger_accept_invalid_certs(true)
+            .build()
+            .expect("Error creating client");
+        let mymutex: Mutex<Option<String>> = Mutex::new(None);
+        let myarc = Arc::new(mymutex);
+        let mymutex2: Mutex<Option<UiResponse>> = Mutex::new(None);
+        let myarc2 = Arc::new(mymutex2);
+
+        X1 {
+            addr: addr.to_string(),
+            user: user.to_string(),
+            password: password.to_string(),
+            client,
+            token: myarc,
+            ui: myarc2,
+            lights: Lights::new(),
+        }
+    }
+
+    pub async fn connect(&'a self) {
+        let body = "{\"client\":\"de.madone.x1client\"}";
+        let addr = self.addr.clone();
+        let token_json_str = self
+            .client
+            .post(format!("https://{addr}/api/clients"))
+            .basic_auth(self.user.clone(), Some(self.password.clone()))
+            .body(body)
+            .send()
+            .await
+            .expect("Error sending post for auth")
+            .text()
+            .await
+            .expect("Error getting text int auth");
+        let token_hash: HashMap<String, String> =
+            serde_json::from_str(token_json_str.as_str()).expect("invalid json");
+        let token = token_hash
+            .get("token")
+            .expect("getting json from response failed");
+        let myarc = self.token.clone();
+        let mut mymutex = myarc.lock().expect("could not lock the mutex");
+        *mymutex = Some(token.to_owned());
+
+        //self.get_ui().await;
+        //self.create_lights();
+    }
+
+    pub fn get_token(&self) -> Option<String> {
+        let myarc = self.token.clone();
+        let mymutex = myarc.lock().expect("could not lock the mutex");
+        mymutex.clone()
+    }
+
+    pub async fn get_ui(&self) -> String {
+        let token = self
+            .get_token()
+            .expect("failed to get token. Not connected?");
+        let addr = self.addr.clone();
+        let resp = self.client
+        .get(
+            format!("https://{addr}/api/v2/uiconfig?expand=[dataPointFlasgs,parameters,locations,trades&token={token}"),
+        )
+        .send()
+        .await
+        .expect("failed to get response of UI")
+        .text()
+        .await
+        .expect("failed to get text of UI response");
+        let myresp: UiResponse = serde_json::from_str(&resp).unwrap();
+        let myarc = self.ui.clone();
+        let mut mymutex = myarc.lock().expect("could not lock the mutex");
+        *mymutex = Some(myresp);
+        resp
+    }
+
+    pub async fn get_value(&self, uid: String) -> Result<String, reqwest::Error> {
+        let token = self
+            .get_token()
+            .expect("Error geting token. Not logged in?");
+        let addr = self.addr.clone();
+        let resp = self
+            .client
+            .get(format!("https://{addr}/api/v2/values/{uid}?token={token}"))
+            .send()
+            .await?
+            .text()
+            .await?;
+        Ok(resp)
+    }
+
+    pub async fn set_value(&self, uid: String, value: u16) -> Result<String, reqwest::Error> {
+        let token = self.get_token().expect("Error getting token");
+        let addr = self.addr.clone();
+        let body = format!(
+            "{{
+                \"values\": [
+                    {{
+                        \"uid\": \"{uid}\",
+                        \"value\": {value}
+                    }}
+                ]
+            }}"
+        );
+        let resp = self
+            .client
+            .put(format!("https://{addr}/api/v2/values?token={token}"))
+            //.basic_auth("MadOne", Some("5315herb"))
+            .body(body)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        Ok(resp)
+    }
+
+    pub fn create_lights(&'a self) {
+        let myarc = self.ui.clone();
+        let mymutex = myarc.lock().expect("could not lock the mutex");
+        let ui = mymutex.clone();
+        let uii = ui.expect("Error getting ui repsonse from mutex");
+        for function in uii.functions {
+            match function.channelType.as_str() {
+                "de.gira.schema.channels.DimmerWhite" => {
+                    let myswitch = Switch {
+                        x1: &self,
+                        uid: function.dataPoints[0].uid.clone(),
+                        val: 0,
+                    };
+                    let mydimmer = Dimm {
+                        x1: &self,
+                        uid: function.dataPoints[1].uid.clone(),
+                        val: 0,
+                    };
+                    let mytuner = ColorTemp {
+                        x1: &self,
+                        uid: function.dataPoints[2].uid.clone(),
+                        val: 0,
+                    };
+                    let mylight = TunableLight {
+                        x1: &self,
+                        name: function.displayName,
+                        switch: myswitch,
+                        dimmer: mydimmer,
+                        tuner: mytuner,
+                    };
+                    self.lights.tunable.add(mylight);
+                    //self.lights.lock().unwrap().tunable.push(mylight);
+                    //lights.tunable.push(mylight);
+                    //println!("Added light")
+                }
+                "de.gira.schema.channels.Switch" => {
+                    let myswitch = Switch {
+                        x1: &self,
+                        uid: function.dataPoints[0].uid.clone(),
+                        val: 0,
+                    };
+
+                    let mylight = SwitchedLight {
+                        x1: &self,
+                        name: function.displayName,
+                        switch: myswitch,
+                    };
+                    self.lights.switchable.add(mylight);
+                    //self.lights.lock().unwrap().switchable.push(mylight);
+                    //println!("Added light")
+                }
+                "de.gira.schema.channels.BlindWithPos" => (),
+                _ => (),
+            }
+        }
+    }
+}
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Function {
+    channelType: String,
+    dataPoints: Vec<DataPoint>,
+    displayName: String,
+    functionType: String,
+    uid: String,
+}
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct DataPoint {
+    name: String,
+    uid: String,
+}
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Location {
+    displayName: String,
+    functions: Option<Vec<String>>,
+    locationType: String,
+    locations: Option<Vec<Location>>,
+}
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Trade {
+    displayName: String,
+    functions: Option<Vec<String>>,
+    tradeType: String,
+}
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UiResponse {
+    functions: Vec<Function>,
+    locations: Vec<Location>,
+    trades: Vec<Trade>,
+}
